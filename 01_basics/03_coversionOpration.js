f0f001ae2dfcf354b6bcb4d11325d9845abbeb14
MdMizanalfiyaz <128036678+MdMizanalfiyaz@users.noreply.github.com>
2024-02-21 18:25:03 +0000

    conversion_Datatype

---
+++ 01_basics/03_coversionOpration.js
@@ -0,0 +1,36 @@
+// let score = "false"
+
+// console.log(typeof score);
+// console.log(typeof (score));
+
+
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+
+
+// "33" => 33
+// "33abc" => NaN(type will be number)
+// true => 1 ; false => 0
+
+
+let isLoggedIn = "Mahim"
+
+
+let booleanisLoggedIn = Boolean(isLoggedIn)
+
+// console.log(booleanisLoggedIn)
+
+
+// 1 => true,0 => false
+//"" => false
+//"Mahim" => true
+
+
+let anotherNumber = 56
+
+let stringNumber = String(anotherNumber)
+
+console.log(stringNumber);
+console.log(typeof stringNumber);
